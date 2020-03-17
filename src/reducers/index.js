b2bf0ebc016a61817bcaf9735ccb0e97f662685c
PUTRIDAMAYANTI\Putri Damayanti <putri@vi8e.com>
2020-03-18 05:52:46 +0700

    Add ads reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import ProductReducer from './ProductReducer';
+import AdsReducer from './AdsReducer';
 
 const reducers = {
     productStore: ProductReducer,
+    adsStore    : AdsReducer
 };
 
 const rootReducer = combineReducers(reducers);
