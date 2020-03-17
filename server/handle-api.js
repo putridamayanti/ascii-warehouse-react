a2acc2a553fc91a47b1de924c12302ae8146acc9
PUTRIDAMAYANTI\Putri Damayanti <putri@vi8e.com>
2020-03-18 05:50:50 +0700

    Add sort by size and price to server

--- server/handle-api.js
+++ server/handle-api.js
@@ -36,25 +36,33 @@ function getQueryParam(url) {
     return params;
 }
 
+function createFacesItem(i, sort) {
+    var item = {
+        id: i + '-' + getRandomString(),
+        size: getRandomInRange(12, 40),
+        price: getRandomInRange(1, 1000),
+        face: faces[i % faces.length],
+        date: new Date(Date.now() - getRandomInRange(1, 1000 * 3600 * 24 * 15)).toString()
+    };
+
+    if (sort === 'size') {
+        item.size = Math.min(42, 12 + Math.floor(i * 0.05));
+    } else if (sort === 'price') {
+        item.price = Math.min(1000, Math.floor(i * 0.1)+1);
+    }
+
+    return item;
+}
+
 module.exports = (req, res, next) => {
     if (req.url.indexOf('api') !== -1) {
         const params = getQueryParam(req.url);
-        // console.log(params);
-        if (!params.limit) {
-            params.limit = 20;
-        }
+        const data = { products: [] };
 
-        const data = { products: [] },
-            facesLen = faces.length;
-
-        for (let i = 0; i < params.limit; i++) {
-            data.products.push({
-                id: getRandomInRange(0, 100000) + '-' + getRandomString(),
-                size: getRandomInRange(12, 40),
-                price: getRandomInRange(1, 1000),
-                face: faces[i % facesLen],
-                date: new Date(Date.now() - getRandomInRange(1, 1000 * 3600 * 24 * 15)).toString()
-            });
+        if (params.skip && params.skip < 10) {
+            for (let i = 0; i < params.limit; i++) {
+                data.products.push(createFacesItem(i+params.skip, params.sort));
+            }
         }
 
         res.writeHead(200, {
--- src/screens/ProductIndex.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-class ProductIndex extends React.Component {
-
-}
