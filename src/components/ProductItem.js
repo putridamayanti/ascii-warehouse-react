d787bc0290a267ef9f8dfb3f93de9feaa374f85f
PUTRIDAMAYANTI\Putri Damayanti <putri@vi8e.com>
2020-03-18 06:07:25 +0700

    Change format price

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -8,12 +8,15 @@ export default class ProductItem extends React.Component {
 
     render() {
         const { size, face, price, date } = this.props;
-
+        var priceFormated =  new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        });
         return (
             <Card style={{ height: 150, marginBottom: 15 }}>
                 <Card.Body>
                     <Card.Title style={{ fontSize: size }}>{ face }</Card.Title>
-                    <Card.Text>${ price }</Card.Text>
+                    <Card.Text>{ priceFormated.format(price)}</Card.Text>
                 </Card.Body>
                 <Card.Footer style={{ fontSize: 12 }}>
                     <Row>
